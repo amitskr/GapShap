0577ea91552470faa5a7337fddf839fdd6f42474
Amit Sarkar <amitskr6@gmail.com>
2023-02-10 12:59:26 +0530

    Splash Screen Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,12 +11,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Gapshap',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: AnimatedSplashScreen(
+            duration: 3000,
+            splash: Image.asset('assets/images/Gapshap.png'),
+            nextScreen: const MyHomePage(),
+            splashTransition: SplashTransition.fadeTransition,
+            backgroundColor: Color.fromARGB(255, 255, 255, 255)));
   }
 }
 
