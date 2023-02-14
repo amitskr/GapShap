49605454d475d023a51458ff332a58e4ce2d70ae
Amit Sarkar <amitskr6@gmail.com>
2023-02-14 10:42:16 +0530

    Login screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+//import 'firebase_options.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,29 +22,101 @@ class MyApp extends StatelessWidget {
         home: AnimatedSplashScreen(
             duration: 2000,
             splash: Image.asset('assets/images/Gapshap.png'),
-            nextScreen: const MyHomePage(),
+            nextScreen: const LoginView(),
             splashTransition: SplashTransition.scaleTransition,
             backgroundColor: const Color.fromARGB(255, 255, 255, 255)));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class RegisterView extends StatefulWidget {
+  const RegisterView({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<RegisterView> createState() => _RegisterViewState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _RegisterViewState extends State<RegisterView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("GapShap"),
       ),
-      body: const Center(child: Text("Shruti Vats Chutiya Hai")),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => print("Yamete Kudasai"),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          //options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              return Column(
+                children: [
+                  TextField(
+                    autocorrect: false,
+                    keyboardType: TextInputType.emailAddress,
+                    controller: _email,
+                    decoration: const InputDecoration(hintText: "Email>>"),
+                  ),
+                  TextField(
+                    obscureText: true,
+                    autocorrect: false,
+                    controller: _password,
+                    decoration: const InputDecoration(hintText: "Password>>"),
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      Firebase.initializeApp(
+                       // options: DefaultFirebaseOptions.currentPlatform,
+                      );
+                      final email = _email.text;
+                      final password = _password.text;
+
+                      final userCredential = await FirebaseAuth.instance
+                          .createUserWithEmailAndPassword(
+                              email: email, password: password);
+
+                      print(userCredential);
+                    },
+                    child: const Text("Register"),
+                  ),
+                ],
+              );
+
+            default:
+              return const Text("Loding...");
+          }
+        },
       ),
     );
   }
